164d0e14a0cdcc9a17d8383054f6c77437bf6155
jmframe <jonframe@gmail.com>
2023-11-22 09:19:44 -0800

    shell script to ensemble models

---
+++ 2023_train_lstm_for_nextgen/ensemble_model_results.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# DIR1="./runs/PC1_Attributes_0711_214955"
+# DIR2="./runs/PC1_Attributes_seed1_0811_212309"
+# nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/PC1_Attributes --metrics NSE MSE KGE
+
+DIR1="./runs/HA_10random_Attributes_a_0711_222711"
+DIR2="./runs/HA_10random_Attributes_a_seed1_0811_220021"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/HA_10random_Attributes_a --metrics NSE MSE KGE
+
+DIR1="./runs/HA_10random_Attributes_b_0711_223123"
+DIR2="./runs/HA_10random_Attributes_b_seed1_0811_220427"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/HA_10random_Attributes_b --metrics NSE MSE KGE
+
+DIR1="./runs/HA_10random_Attributes_c_0711_223533"
+DIR2="./runs/HA_10random_Attributes_c_seed1_0811_220833"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/HA_10random_Attributes_c --metrics NSE MSE KGE
+
+DIR1="./runs/HA_10random_Attributes_d_0711_223535"
+DIR2="./runs/HA_10random_Attributes_d_seed1_0811_220835"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/HA_10random_Attributes_d --metrics NSE MSE KGE
+
+DIR1="./runs/HA_10random_Attributes_e_0711_223946"
+DIR2="./runs/HA_10random_Attributes_e_seed1_0811_221242"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/HA_10random_Attributes_e --metrics NSE MSE KGE
+
+DIR1="./runs/HA_10random_Attributes_f_0711_223947"
+DIR2="./runs/HA_10random_Attributes_f_seed1_0811_221243"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/HA_10random_Attributes_f --metrics NSE MSE KGE
+
+DIR1="./runs/HA_10random_Attributes_g_0711_224357"
+DIR2="./runs/HA_10random_Attributes_g_seed1_0811_221647"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/HA_10random_Attributes_g --metrics NSE MSE KGE
+
+DIR1="./runs/HA_10random_Attributes_h_0711_224359"
+DIR2="./runs/HA_10random_Attributes_h_seed1_0811_221649"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/HA_10random_Attributes_h --metrics NSE MSE KGE
+
+DIR1="./runs/HA_10random_Attributes_i_0711_224401"
+DIR2="./runs/HA_10random_Attributes_i_seed1_0811_221651"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/HA_10random_Attributes_i --metrics NSE MSE KGE
+
+DIR1="./runs/NOAHMP_Attributes_0711_214556"
+DIR2="./runs/NOAHMP_Attributes_seed1_0811_211909"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/NOAHMP_Attributes --metrics NSE MSE KGE
+
+DIR1="./runs/PC2_Attributes_0711_215406"
+DIR2="./runs/PC2_Attributes_seed1_0811_212720"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/PC2_Attributes --metrics NSE MSE KGE
+
+DIR1="./runs/PC3_Attributes_0711_215817"
+DIR2="./runs/PC3_Attributes_seed1_0811_213133"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/PC3_Attributes --metrics NSE MSE KGE
+
+DIR1="./runs/PC4_Attributes_0711_220217"
+DIR2="./runs/PC4_Attributes_seed1_0811_213532"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/PC4_Attributes --metrics NSE MSE KGE
+
+DIR1="./runs/PC5_Attributes_0711_220627"
+DIR2="./runs/PC5_Attributes_seed1_0811_213942"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/PC5_Attributes --metrics NSE MSE KGE
+
+DIR1="./runs/PC6_Attributes_0711_221035"
+DIR2="./runs/PC6_Attributes_seed1_0811_214354"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/PC6_Attributes --metrics NSE MSE KGE
+
+DIR1="./runs/PC7_Attributes_0711_221447"
+DIR2="./runs/PC7_Attributes_seed1_0811_214804"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/PC7_Attributes --metrics NSE MSE KGE
+
+DIR1="./runs/PC8_Attributes_0711_221848"
+DIR2="./runs/PC8_Attributes_seed1_0811_215202"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/PC8_Attributes --metrics NSE MSE KGE
+
+DIR1="./runs/PC9_Attributes_0711_222258"
+DIR2="./runs/PC9_Attributes_seed1_0811_215613"
+nh-results-ensemble --run-dirs $DIR1 $DIR2 --output-dir ./runs/PC9_Attributes --metrics NSE MSE KGE
