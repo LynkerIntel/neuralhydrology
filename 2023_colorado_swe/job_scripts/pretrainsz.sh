ceef87323b8ac942a28f98070d104c27af20387f
Josh Sturtevant <joshsturtevant@JoshSturtevant.localdomain>
2024-06-12 14:39:47 -0600

    reorg files

--- 2023_colorado_swe/job_scripts/pretrainsz.sh
+++ 2023_colorado_swe/job_scripts/pretrainsz.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 nh-run train --config-file ./config_files/dev_pretrain_finetune/pretrain_snotel.yml 
-nh-run train --config-file ./config_files/dev_pretrain_finetune/pretrain_NOswe.yml 
+#nh-run train --config-file ./config_files/dev_pretrain_finetune/pretrain_NOswe.yml 
